9812d7d0a41aefb5a323cde3c64f57b453e725bc
codebyahadul <ahadul.prof@gmail.com>
2024-08-11 23:06:43 +0600

    added axios secure for secure api

--- src/hooks/useCurrentUser.jsx
+++ src/hooks/useCurrentUser.jsx
@@ -1,15 +1,15 @@
 import { useContext } from 'react';
-import useAxiosCommon from './userAxiosCommon';
 import { useQuery } from '@tanstack/react-query';
 import { AuthContext } from '../provider/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
 
 const useCurrentUser = () => {
-    const axiosCommon = useAxiosCommon()
+    const axiosSecure = useAxiosSecure()
     const { user } = useContext(AuthContext)
     const { data: currentUser = {}, refetch } = useQuery({
         queryKey: ['user', 'profile', `${user?.mobile}`],
         queryFn: async () => {
-            const { data } = await axiosCommon.get(`/user/${user?.mobile ? user?.mobile: user?.email}`)
+            const { data } = await axiosSecure.get(`/user/${user?.mobile ? user?.mobile: user?.email}`)
             return data;
         }
     })
